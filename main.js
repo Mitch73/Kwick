37b7552eecd25a9fe656fbcd48875007999a2228
mich <djackson2284@gmail.com>
2021-12-30 17:18:03 +0100

    ...

--- main.js
+++ main.js
@@ -6,57 +6,100 @@ $(document).ready(function() {
     });
     ping.done(function(data) {
         // console.log(data);
-        let user = {
+        let utilisateur = {
             ready: data['result']['ready'],
         }
     });
     // section formulaire s'incrire
-    $("#submit").click(function(e){
+        $("#signUp").on('click', function(e){
+            e.preventDefault();
+            let name = $('#name').val();
+            let password = $('#password').val();
+            let confirmPassword = $('#retapez_le_mot_de_passe').val();
+
+            let inscription = $.ajax({
+                url: 'http://greenvelvet.alwaysdata.net/kwick/api/signup/' + name + ' ' + password + ' ' + confirmPassword,
+                dataType: 'jsonp'
+        });  
+    
+        inscription.done(function(data){
+            // console.log(data);
+            let utilisateur = {
+                // token represente les identifiants créers par l'utilisateur un peu comme une carte d'indentité
+                id: data['result']['id'],            
+                token: data['result']['token']
+            }
+            // la méthode JSON.stringify() permet de convertir les valeur de js en JSON et les stocker dans le localStoragesetItem()
+            let json_data = JSON.stringify(utilisateur);
+            localStorage.setItem('utilisateur', json_data); 
+
+            if (data['result']['status'] === 'done') {
+                window.location = 'messagerie.html';
+            } else {
+                $('.msg').fadeIn();
+            }
+        })             
+    });
+    //connexion de l'utilisateur
+    $("#logIn").on('click', function(e) {
         e.preventDefault();
+        let nameLogIn = $('#name').val();
+        let passwordLogIn = $('#password').val();
 
-        let name = $('#name').val();
-        let password = $('#password').val();
-        let confirmPassword = $('#retapez_le_mot_de_passe').val();
-
-        let inscription = $.ajax({
-            url: 'http://greenvelvet.alwaysdata.net/kwick/api/signup/' + name + '/' + password + '/' + confirmPassword,
+        let connect = $.ajax({
+            url: 'http://greenvelvet.alwaysdata.net/kwick/api/login/' + nameLogIn + ' ' + passwordLogIn,
             dataType: 'jsonp'
+        });
+
+        connect.done(function(data) {
+            // console.log(data);
+
+            let utilisateur = {
+                id: data['result']['id'],
+                token: data['result']['token']
+            }
+
+            let json_data = JSON.stringify(utilisateur);
+            localStorage.setItem('utilisateur', json_data);
+
+            if (data['result']['status'] === 'done') {
+                window.location = 'messagerie.html';
+            } else {
+                $('.msg').fadeIn();
+            }
+
+        })
     });
 
-    inscription.done(function(data){
-        
-    })
+});
 
-    $('#password, #retapez_le_mot_de_passe').on('keyup', function () {
-        
-        if ($('#password').val() == $('#retapez_le_mot_de_passe').val()) {
-          $('#message').html('Matching').css('color', 'green');
-        } else 
-          $('#message').html('Not Matching').css('color', 'red');
-      });
-}
+// $('#password, #retapez_le_mot_de_passe').on('keyup', function () {        
+//     if ($('#password').val() == $('#retapez_le_mot_de_passe').val()) {
+//       $('#message').html('Matching').css('color', 'green');
+//     } else 
+//       $('#message').html('Not Matching').css('color', 'red');
+//   });
 
+// section formulaire s'incrire
 
-// // section formulaire s'incrire
-
-// // je recupère mon formulaire document.getElementById() dont la variable btnInscrire
+// je recupère mon formulaire document.getElementById() dont la variable btnInscrire
 // let btnInscrire = document.getElementById('signIn');
 // let password = document.getElementById('password');
 // let confirmPassword = document.getElementById('retapez_le_mot_de_passe');
 
 // btnInscrire.addEventListener('submit', (e) => {
 //     e.preventDefault();
-//     let fields = document.querySelectorAll('input[required]');
-//     // cette boucle permet de parcourire un par par les champs et supprimers les messages d'erreurs quand elles sont remplies
-//     fields.forEach((field) => {resetField(field)});
-//     // cette variable de vient false si l'un des trois n'est pas valide
-//     let valid = true;
+    // let fields = document.querySelectorAll('input[required]');
+    // cette boucle permet de parcourire un par par les champs et supprimers les messages d'erreurs quand elles sont remplies
+    // fields.forEach((field) => {resetField(field)});
+    // cette variable de vient false si l'un des trois n'est pas valide
+    // let valid = true;
 
-//     // la boucle forEach permet de verifier la validité de chacun des champs
-//     // permet de d'executer une function a fleche pour chacun des éléments stocker dans la variable fields 
-//     // la renvoye une valuer boolean a l'occurance true donc j'applique une condition if
-//     // si validateField revoye true a la ligne 29 la ligne 14 sera donc false grace au point d'exclamation et la 15 ne sera donc pas executer
-//     // et la valuer valid va rester a sa valuer initial de true
+    // la boucle forEach permet de verifier la validité de chacun des champs
+    // permet de d'executer une function a fleche pour chacun des éléments stocker dans la variable fields 
+    // la renvoye une valuer boolean a l'occurance true donc j'applique une condition if
+    // si validateField revoye true a la ligne 29 la ligne 14 sera donc false grace au point d'exclamation et la 15 ne sera donc pas executer
+    // et la valuer valid va rester a sa valuer initial de true
 //     fields.forEach((field) => {
 //        if(!validateField(field)){
 //            valid = false;
@@ -67,10 +110,10 @@ $(document).ready(function() {
 //     }
 // }, false);
 
-// // cette function resoit en entrée un champ et va utiliser une méthod de cette objet field qui s'appel checkValidity()
-// // cette méthod checkValidity() va retouner soit true si le champs est valid et false s'il ne l'est pas
-// // et pour ce elle va ce servir des attributes required dans le html
-// // puisqu'elle me renvoy false j'ai décider de faire une condition if
+// cette function resoit en entrée un champ et va utiliser une méthod de cette objet field qui s'appel checkValidity()
+// cette méthod checkValidity() va retouner soit true si le champs est valid et false s'il ne l'est pas
+// et pour ce elle va ce servir des attributes required dans le html
+// puisqu'elle me renvoy false j'ai décider de faire une condition if
 // function validateField(field){
 //     if(field.checkValidity()){
 //         return true;
@@ -81,7 +124,7 @@ $(document).ready(function() {
 //     }
 // }
 
-// // function supprime les messages d'erreur quand les champs sont remplies 
+// function supprime les messages d'erreur quand les champs sont remplies 
 // function resetField(field){
 //     let fieldLabel = field.previousElementSibling;
 //     field.classList.remove('invalid');
@@ -91,7 +134,7 @@ $(document).ready(function() {
 //     field.valid = true;
 // }
 
-// // pasword settings avec les variable password et confirmPassword
+// pasword settings avec les variable password et confirmPassword
 // function validatePassword(){
 //     if(password.value != confirmPassword.value) {
 //         confirmPassword.setCustomValidity("Passwords Don't Match");
@@ -103,35 +146,35 @@ $(document).ready(function() {
 //   password.onchange = validatePassword;
 //   confirmPassword.onkeyup = validatePassword;
 
-// // let check = function checkPassword() {
-// //     if (document.getElementById('password').value ==
-// //       document.getElementById('retapez_le_mot_de_passe').value) {
-// //       document.getElementById('message').style.color = 'green';
-// //       document.getElementById('message').innerHTML = 'matching';
-// //       return true;
-// //     } else {
-// //       document.getElementById('message').style.color = 'red';
-// //       document.getElementById('message').innerHTML = 'not matching';
-// //       return false;
-// //     //   document.getElementById('submit').e.preventDefault();
-// //     }
-// //   }
+// let check = function checkPassword() {
+//     if (document.getElementById('password').value ==
+//       document.getElementById('retapez_le_mot_de_passe').value) {
+//       document.getElementById('message').style.color = 'green';
+//       document.getElementById('message').innerHTML = 'matching';
+//       return true;
+//     } else {
+//       document.getElementById('message').style.color = 'red';
+//       document.getElementById('message').innerHTML = 'not matching';
+//       return false;
+//     //   document.getElementById('submit').e.preventDefault();
+//     }
+//   }
 
-// // function checkPassword(){
-// //     if(document.getElementById('password').value == 'hello'){
-// //     //  alert('Correct Password!'); 
-// //        location.href = "privée.html";
-// //       } else {
-// //     //   alert('Wrong Password!');
-// //        return false;
-// //      }
-// //     }
+// function checkPassword(){
+//     if(document.getElementById('password').value == 'hello'){
+//     //  alert('Correct Password!'); 
+//        location.href = "privée.html";
+//       } else {
+//     //   alert('Wrong Password!');
+//        return false;
+//      }
+//     }
 
-// // section formulaire connecter
-// // let btnConnecter = document.getElementById('logIn');
+// section formulaire connecter
+// let btnConnecter = document.getElementById('logIn');
 
-// // btnConnecter.addEventListener("submit", function logIn(e){
-// //     e.preventDefault();
-// //     let champs = document.querySelectorAll('input[required]');
+// btnConnecter.addEventListener("submit", function logIn(e){
+//     e.preventDefault();
+//     let champs = document.querySelectorAll('input[required]');
 
-// // }, false);
+// }, false);
